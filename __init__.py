2897b8796c54ef21743581ec185972f617053a4c
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2021-10-10 16:55:22 +0200

    create LOD ui

--- __init__.py
+++ __init__.py
@@ -35,20 +35,28 @@ class SimplifyPropertyGroup(bpy.types.PropertyGroup):
         description="",
         default=5000,
         min=1,
-        )
+    )
+
+class LODPropertyGroup(bpy.types.PropertyGroup):
+    target_face_number: bpy.props.IntProperty( 
+        name="Target Face Number",
+        description="",
+        default=5000,
+        min=1,
+    )
     ratio: bpy.props.FloatProperty( 
         name="Ratio per step",
         description="",
         default=0.5,
         min=0,
         max=1,
-        )
-    target_face_number: bpy.props.IntProperty( 
-        name="Target Face Number",
+    )
+    min_face_number: bpy.props.IntProperty( 
+        name="Lowest Face Number",
         description="",
-        default=5000,
+        default=50,
         min=1,
-        )
+    )
     
 
 # =================================================================
@@ -95,6 +103,39 @@ class OP_SIMPLIFY(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class OP_LOD(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "cgb_model.create_lods"
+    bl_label = "Create LODs"
+
+    target_face_number: bpy.props.IntProperty( 
+        name="Target Face Number",
+        description="",
+        default=5000,
+        min=1,
+    )
+    ratio: bpy.props.FloatProperty( 
+        name="Ratio per step",
+        description="",
+        default=0.5,
+        min=0,
+        max=1,
+    )
+    min_face_number: bpy.props.IntProperty( 
+        name="Target Face Number",
+        description="",
+        default=5000,
+        min=1,
+    )
+
+    def execute(self, context):
+        print(self.target_face_number)
+        print(self.ratio)
+        print(self.min_face_number)
+
+        return {'FINISHED'}
+
+
 # =================================================================
 # Panels
 
@@ -114,6 +155,8 @@ class PANEL_PREPARE(bpy.types.Panel):
         else:
             row.label(text='No active selection')
 
+
+
 class PANEL_SIMPLIFY(bpy.types.Panel):
     bl_label = "Simplify"
     bl_category = "cgbookcase Mesh Processing"
@@ -124,7 +167,7 @@ class PANEL_SIMPLIFY(bpy.types.Panel):
         layout = self.layout
         options = context.scene.simplifyPropertyGroupInstance
         
-        col = layout.column(align=True)
+        col = layout.column()
 
         row = col.row()
         row.prop(options, "target_face_number")
@@ -132,6 +175,37 @@ class PANEL_SIMPLIFY(bpy.types.Panel):
         row = col.row()
         props = row.operator("cgb_model.simplify", icon="MOD_SIMPLIFY")
         props.target_face_number = options.target_face_number
+
+
+
+class PANEL_LOD(bpy.types.Panel):
+    bl_label = "LODs"
+    bl_category = "cgbookcase Mesh Processing"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    def draw(self, context):
+        layout = self.layout
+        options = context.scene.lodPropertyGroupInstance
+        
+        col = layout.column(align=True)
+
+        row = col.row()
+        row.prop(options, "target_face_number")
+
+        row = col.row()
+        row.prop(options, "ratio")
+
+        row = col.row()
+        row.prop(options, "min_face_number")
+
+        col = layout.column(align=True)
+
+        row = col.row()
+        props = row.operator("cgb_model.create_lods", icon="FORCE_LENNARDJONES")
+        props.target_face_number = options.target_face_number
+        props.ratio = options.target_face_number
+        props.min_face_number = options.target_face_number
 
 
 
@@ -146,7 +220,7 @@ class PANEL_BAKE(bpy.types.Panel):
         
         row = layout.row()
 
-        row.operator("cgb_model.simplify", icon="MOD_SIMPLIFY")
+        row.operator("cgb_model.simplify", icon="RENDER_STILL")
 
 
 class PANEL_EXPORT(bpy.types.Panel):
@@ -163,25 +237,24 @@ class PANEL_EXPORT(bpy.types.Panel):
         row.operator("cgb_model.simplify", icon="MOD_SIMPLIFY")
 
 
-
-
 # =================================================================
 
-
-classes = (OP_EXPORT_MESH, PANEL_PREPARE, OP_SIMPLIFY, PANEL_SIMPLIFY, PANEL_BAKE, PANEL_EXPORT, SimplifyPropertyGroup)
+classes = (OP_EXPORT_MESH, PANEL_PREPARE, OP_SIMPLIFY, PANEL_SIMPLIFY, OP_LOD, PANEL_LOD, PANEL_BAKE, PANEL_EXPORT, SimplifyPropertyGroup, LODPropertyGroup)
 
 def register():
     for c in classes:
+        print(c)
         bpy.utils.register_class(c)
 
-    bpy.types.Scene.simplifyPropertyGroupInstance = bpy.props.PointerProperty(
-            type=SimplifyPropertyGroup)
+    bpy.types.Scene.simplifyPropertyGroupInstance = bpy.props.PointerProperty(type=SimplifyPropertyGroup)
+    bpy.types.Scene.lodPropertyGroupInstance = bpy.props.PointerProperty(type=LODPropertyGroup)
 
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
     
     del bpy.types.Scene.simplifyPropertyGroupInstance
+    del bpy.types.Scene.lodPropertyGroupInstance
         
 if __name__ == "__main__":
     register()
